fc510f9ef24124ea67088630aa7082da6e288cec
Ronit Wilson <ronitwilson.rolance@in.bosch.com>
2024-09-06 19:23:35 +0530

    inital changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller,Get } from "@nestjs/common";
 
-@Controller()
+@Controller('report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor() {
+    console.log("AppController created");
+  }
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllIncomeReports() {
+    return [];
+  }
+  @Get(':id')
+  getIncomeReportbyId() { 
+    return {};
   }
 }
